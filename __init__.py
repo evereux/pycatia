e88d6aa956033fd491252ce47ccd0441895f4a6c
evereux <evereux@gmail.com>
2018-12-28 15:04:24 +0000

    Added ability to create points from csv file.
    Included examples.
    
    Extended functionality of document.py classes.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,12 @@
 #! /usr/bin/python3.6
 
 from .catiaapplication import CATIAApplication
+from .csv_tools import create_points
 from .document import Document
+from .document import Documents
 from .general_functions import create_measurable
+from .general_functions import run_system_service
+from .hybridshapefactory import HybridShapeFactory
 from .measurable import CATIAMeasurable
 from .part import Part
 from .product import Product
@@ -14,7 +18,7 @@ __author__ = 'Paul Bourne'
 __author_email = 'evereux@gmail.com'
 __description__ = 'A python module to access the CATIA Measurable object.'
 __name__ = "pycatia"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __url__ = "https://github.com/evereux/pycatia"
 
 name = __name__
