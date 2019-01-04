87853da87d7739f7c599badfb630b63d973cc424
evereux <evereux@gmail.com>
2019-01-04 18:21:36 +0000

    rtfd fix attempt #6

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
         return MagicMock()
 
 
-MOCK_MODULES = ['pywin32',]
+MOCK_MODULES = ['pywin32', 'win32com']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
