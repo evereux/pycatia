98d2e449c0d8dc802f1984335f6233052225d69a
evereux <evereux@gmail.com>
2019-01-06 19:25:26 +0000

    bump version

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ __author__ = 'Paul Bourne'
 __author_email = 'evereux@gmail.com'
 __description__ = 'A python module to access the CATIA Measurable object.'
 __name__ = "pycatia"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __url__ = "https://github.com/evereux/pycatia"
 
 name = __name__
