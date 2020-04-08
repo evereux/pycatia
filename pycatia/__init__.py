e181b6c11572af901352d23176d55183099c3922
evereux <evereux>
2020-04-08 15:52:20 +0100

    correct setup builds and docs for 0.1.9

--- pycatia/__init__.py
+++ pycatia/__init__.py
@@ -6,7 +6,7 @@ __author__ = 'Paul Bourne'
 __author_email = 'evereux@gmail.com'
 __description__ = 'A python module to access the CATIA Measurable object.'
 __name__ = "pycatia"
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __url__ = "https://github.com/evereux/pycatia"
 
 name = __name__
