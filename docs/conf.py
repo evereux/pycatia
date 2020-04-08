b6edf1815338be473e1f383702c3c698bde04ccf
evereux <evereux>
2020-04-08 16:10:40 +0100

    version bump due to pypi naming conflicts

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ copyright = '2018, Paul Bourne'
 author = 'Paul Bourne'
 
 # The short X.Y version
-version = '0.1.9'
+version = '0.2.0'
 # The full version, including alpha/beta/rc tags
 release = version
 
