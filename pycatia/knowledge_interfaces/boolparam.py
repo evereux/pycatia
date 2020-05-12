d45a0958b49b7700f4ddbabf26f4ba0604fb7244
evereux <evereux@gmail.com>
2020-05-12 15:45:29 +0100

    fix create_boolean so returns parameter

--- pycatia/knowledge_interfaces/boolparam.py
+++ pycatia/knowledge_interfaces/boolparam.py
@@ -1,26 +1,49 @@
 #! /usr/bin/python3.6
+# module initially auto generated using V5Automation.chm from CATIA V5 R25
 
 from pycatia.knowledge_interfaces.parameter import Parameter
 
 
 class BoolParam(Parameter):
-    def __init__(self, name=None, value=None, parameter=None, parent=None):
-        super().__init__(parameter)
-        if value and not isinstance(value, bool):
-            raise ValueError(f'Parameter value [{value}] has to be bool()')
+    """
+        .. note::
+            CAA V5 Visual Basic help
 
-        self.parameters = self.set_parameters(parent)
+                | Represents the boolean parameter.The following example shows how to
+                | create it:Dim CATDocs As Documents  Set CATDocs      = CATIA.Documents
+                | Dim part1 As Document  Set part1        = CATDocs.Add("CATPart")
+                | Dim availability As BooleanParam  Set availability =
+                | part1.Parameters.CreateBoolean("availability", True)
 
-        if parameter:
-            self.parameter = parameter
-        else:
-            self.parameter = Parameter(self.parameters.CreateBoolean(name, value))
+    """
 
-    @staticmethod
-    def set_parameters(parent):
-        try:
-            # if parent is <class "Parameters">S
-            return parent.parameters
-        except AttributeError:
-            # if parent is something like Catia.ActiveDocument.Part.ParametersS
-            return parent
+    def __init__(self, com_boolean_parameter):
+        super().__init__(com_boolean_parameter)
+
+    @property
+    def value(self):
+        """
+        .. note::
+            CAA V5 Visual Basic help
+
+                | Value
+                | o Property Value(    ) As boolean
+                |
+                | Returns or sets the value of the boolean parameter.  Example: This
+                | example sets the availability boolean parameter value to True if its
+                | value is False:  If (availability.Value = False)  Then
+                | availability.Value = True End If
+
+
+                | Parameters:
+
+
+        """
+        return self.parameter.Value
+
+    @value.setter
+    def value(self, value):
+        self.parameter.value = value
+
+    def __repr__(self):
+        return f'BoolParam(name="{self.name}". value="{self.value}")'
