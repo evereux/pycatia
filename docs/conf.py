4f99bcd1a5052e32f44fca428ed7a5b834bd3efb
evereux <evereux@gmail.com>
2020-06-19 18:47:32 +0100

    fix pypi packaging issue and removed redundant macros

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ copyright = '2020, Paul Bourne'
 author = 'Paul Bourne'
 
 # The short X.Y version
-version = '0.3.3'
+version = '0.3.4'
 # The full version, including alpha/beta/rc tags
 release = version
 
