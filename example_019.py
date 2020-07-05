5cc85d081488cb766398ee9feb71a026b992b548
evereux <evereux@gmail.com>
2020-07-05 12:03:01 +0100

    new example using search tool and changing visual properties

---
+++ example_019.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3.6
+
+"""
+
+    Example 19:
+
+    Searching and changing visual properties. Find all Red points and make them
+    Pink.
+
+"""
+
+from pycatia import catia
+from pycatia.enumeration.enumeration_types import cat_vis_property_status
+
+document = catia.active_document
+selection = document.selection
+selection.search("CATGmoSearch.Point.Color='(255,0,0)',in")
+
+# how many items have been found?
+catia.logger.info(f'Found {selection.count2} items.')
+
+# to loop through the items
+for i in range(selection.count2):
+    item = selection.item2(i + 1)
+    type = item.type
+    value = item.value
+    reference = item.reference
+    print(item, type, value, reference)
+
+# change the colour of all the selection red points to blue.
+vis_properties = selection.vis_properties
+real_colour = vis_properties.get_real_color()
+s, r, g, b = real_colour
+# s represents the enum cat_vis_property_status.
+# >>> cat_vis_property_status[s]
+# catVisPropertyDefined # this means all colours in selection are the same.
+# >>> cat_vis_property_status[s]
+# catVisPropertyUnDefined # this means all colours in selection are NOT the same.
+# r, g, b are the red, green blue values returned by get_real_color since we
+# searched for all red points r == 255, g == 0 and b == 0.
+
+# Let's change all the selected red points pink with no inheritance.
+vis_properties.set_real_color(255, 128, 255, 0)
