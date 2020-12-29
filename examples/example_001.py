574c522a3f8c3d34370ef10d4f98cd4b7a0f0575
evereux <evereux@gmail.com>
2020-12-29 17:49:39 +0000

    added description about sys.path insertion

--- examples/example_001.py
+++ examples/example_001.py
@@ -10,11 +10,11 @@
 
 ##########################################################
 # insert syspath to project folder so examples can be run.
+# for development purposes.
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath('..\\pycatia'))
-
 ##########################################################
 
 from pycatia import catia
