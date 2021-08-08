51c09644affbd9957cdd2cc12fe434a7426e375f
evereux <evereux@gmail.com>
2021-08-08 17:50:22 +0100

    bump version numbers

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ copyright = '2020, Paul Bourne'
 author = 'Paul Bourne'
 
 # The short X.Y version
-version = '0.5.0'
+version = '0.5.1'
 # The full version, including alpha/beta/rc tags
 release = version
 
