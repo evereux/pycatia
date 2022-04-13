111b863faf9f8560f02e3d82c5d8edcd56dd476a
evereux <evereux@gmail.com>
2022-04-13 12:56:07 +0100

    added 3.9 and 3.10 versions

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
     ],
