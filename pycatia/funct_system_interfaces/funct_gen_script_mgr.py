afea10f50fd84a240998b3af62b3ac3273a738bb
evereux <evereux@gmail.com>
2022-08-19 09:35:56 +0100

    funct_system_interfaces preliminary commit

---
+++ pycatia/funct_system_interfaces/funct_gen_script_mgr.py
@@ -0,0 +1,82 @@
+#! usr/bin/python3.6
+"""
+    Module initially auto generated using V5Automation files from CATIA V5 R28 on 2020-09-25 14:34:21.593357
+
+    .. warning::
+        The notes denoted "CAA V5 Visual Basic Help" are to be used as reference only.
+        They are there as a guide as to how the visual basic / catscript functions work
+        and thus help debugging in pycatia.
+        
+"""
+from pycatia.funct_system_interfaces.funct_scripts import FunctScripts
+from pycatia.funct_system_interfaces.functional_facet import FunctionalFacet
+
+
+class FunctGenScriptMgr(FunctionalFacet):
+    """
+        .. note::
+            :class: toggle
+
+            CAA V5 Visual Basic Help (2020-09-25 14:34:21.593357)
+
+                | System.IUnknown
+                |     System.IDispatch
+                |         System.CATBaseUnknown
+                |             System.CATBaseDispatch
+                |                 System.AnyObject
+                |                     CATFunctSystemItf.FunctionalFacet
+                |                         FunctGenScriptMgr
+                | 
+                | The interface to access a Functional GenerativeScripts
+                | manager.
+    
+    """
+
+    def __init__(self, com_object):
+        super().__init__(com_object)
+        self.funct_gen_script_mgr = com_object
+
+    @property
+    def current_script(self) -> int:
+        """
+        .. note::
+            :class: toggle
+
+            CAA V5 Visual Basic Help (2020-09-25 14:34:21.593357)
+                | o Property CurrentScript() As long
+                | 
+                |     Get the CurrentScript.
+
+        :return: int
+        :rtype: int
+        """
+
+        return self.funct_gen_script_mgr.CurrentScript
+
+    @current_script.setter
+    def current_script(self, value: int):
+        """
+        :param int value:
+        """
+
+        self.funct_gen_script_mgr.CurrentScript = value
+
+    @property
+    def scripts(self) -> FunctScripts:
+        """
+        .. note::
+            :class: toggle
+
+            CAA V5 Visual Basic Help (2020-09-25 14:34:21.593357)
+                | o Property Scripts() As FunctScripts (Read Only)
+                | 
+                |     Get the Generative Scripts collection.
+
+        :return: FunctScripts
+        :rtype: FunctScripts
+        """
+
+        return FunctScripts(self.funct_gen_script_mgr.Scripts)
+
+    def __repr__(self):
+        return f'FunctGenScriptMgr(name="{self.name}")'
---
+++ pycatia/funct_system_interfaces/functional_facet.py
@@ -0,0 +1,86 @@
+#! usr/bin/python3.6
+"""
+    Module initially auto generated using V5Automation files from CATIA V5 R28 on 2020-09-25 14:34:21.593357
+
+    .. warning::
+        The notes denoted "CAA V5 Visual Basic Help" are to be used as reference only.
+        They are there as a guide as to how the visual basic / catscript functions work
+        and thus help debugging in pycatia.
+        
+"""
+from pycatia.funct_system_interfaces.functional_element import FunctionalElement
+from pycatia.system_interfaces.any_object import AnyObject
+
+
+class FunctionalFacet(AnyObject):
+    """
+        .. note::
+            :class: toggle
+
+            CAA V5 Visual Basic Help (2020-09-25 14:34:21.593357)
+
+                | System.IUnknown
+                |     System.IDispatch
+                |         System.CATBaseUnknown
+                |             System.CATBaseDispatch
+                |                 System.AnyObject
+                |                     FunctionalFacet
+                | 
+                | The interface to access a Functional Facet.
+    
+    """
+
+    def __init__(self, com_object):
+        super().__init__(com_object)
+        self.functional_facet = com_object
+
+    @property
+    def functional_element(self) -> FunctionalElement:
+        """
+        .. note::
+            :class: toggle
+
+            CAA V5 Visual Basic Help (2020-09-25 14:34:21.593357)
+                | o Property FunctionalElement() As FunctionalElement (Read
+                | Only)
+                | 
+                |     Get the Functional Element owning the Facet.
+
+        :return: FunctionalElement
+        :rtype: FunctionalElement
+        """
+
+        return FunctionalElement(self.functional_facet.FunctionalElement)
+
+    def free(self) -> None:
+        """
+        .. note::
+            :class: toggle
+
+            CAA V5 Visual Basic Help (2020-09-25 14:34:21.593357))
+                | o Sub Free()
+                | 
+                |     Free the resources allocated by the Facet.
+
+        :return: None
+        :rtype: None
+        """
+        return self.functional_facet.Free()
+
+    def init(self) -> None:
+        """
+        .. note::
+            :class: toggle
+
+            CAA V5 Visual Basic Help (2020-09-25 14:34:21.593357))
+                | o Sub Init()
+                | 
+                |     Init resources for the Facet.
+
+        :return: None
+        :rtype: None
+        """
+        return self.functional_facet.Init()
+
+    def __repr__(self):
+        return f'FunctionalFacet(name="{self.name}")'
