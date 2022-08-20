c2589dbbd22622fb08e13d634cb4290ee3aa7fbd
evereux <evereux@gmail.com>
2022-08-20 12:14:43 +0100

    drop support for correction of malformed document types
    "functionalsystem" can't easily be capitolised. i may come back to this and write a proper function to better handle this

--- pycatia/in_interfaces/documents.py
+++ pycatia/in_interfaces/documents.py
@@ -87,15 +87,13 @@ class Documents(Collection):
         :rtype: Document
         """
 
-        document_type_uf = document_type.capitalize()
+        document_types = ['Part', 'Product', 'Drawing', 'FunctionalSystem']
+        if document_type not in document_types:
+            raise ValueError(f'Document type {document_type} must be in {document_types}')
 
-        document_types = ['Part', 'Product', 'Drawing']
-        if document_type_uf not in document_types:
-            raise ValueError(f'Document type must be in [{document_types}]')
+        self.logger.info(f'Creating a new "{document_type}".')
 
-        self.logger.info(f'Creating a new "{document_type_uf}".')
-
-        return Document(self.documents.Add(document_type_uf))
+        return Document(self.documents.Add(document_type))
 
     def count_types(self, file_type_list: list_str) -> int:
         """
