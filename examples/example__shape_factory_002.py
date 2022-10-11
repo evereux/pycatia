3472ead12906722d84b06f091e6b25be7af95758
deloarts <philip.delorenzo+github@outlook.com>
2022-10-11 09:03:52 +0200

    Add example shape_factory_002: Mirror a body

---
+++ examples/example__shape_factory_002.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3.6
+
+"""
+
+    Example - Shape Factory - 002:
+
+    Mirror the main body of the part.
+    This requires CATIA running with an open part with an non-empty main body.
+
+"""
+
+##########################################################
+# insert syspath to project folder so examples can be run.
+# for development purposes.
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..\\pycatia'))
+##########################################################
+
+from pycatia import catia
+from pycatia.mec_mod_interfaces.part_document import PartDocument
+
+caa = catia()
+documents = caa.documents
+doc = PartDocument(caa.active_document.com_object)
+part = doc.part
+part.in_work_object = part.main_body
+
+mirror_reference = part.create_reference_from_object(part.origin_elements.plane_zx)
+shape_factory = part.shape_factory
+mirror_symmetry = shape_factory.add_new_symmetry_2(mirror_reference)
+mirror_symmetry.hybrid_shape
+
+part.in_work_object = part.main_body
+part.update_object(part.main_body)
