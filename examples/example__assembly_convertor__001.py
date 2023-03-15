5500786528426f05e8b9501723338cee42acf8b7
Philip Delorenzo <git@deloarts.com>
2023-03-15 08:46:05 +0100

    Fix typo

--- examples/example__assembly_convertor__001.py
+++ examples/example__assembly_convertor__001.py
@@ -2,7 +2,7 @@
 
 """
 
-    Example - Asssembly Convetor - 001
+    Example - Assembly Convertor - 001
 
     Print the BOM of a product to XLS using the inbuilt AssemblyConvertor. You
     must already have excel installed.
@@ -23,13 +23,13 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..\\pycatia'))
+sys.path.insert(0, os.path.abspath("..\\pycatia"))
 ##########################################################
 from pathlib import Path
 
 from pycatia import catia
-from pycatia.product_structure_interfaces.product import Product
 from pycatia.product_structure_interfaces.assembly_convertor import AssemblyConvertor
+from pycatia.product_structure_interfaces.product import Product
 
 # file_type can be "TXT", "HTML" or "XLS".
 file_type = "XLS"
@@ -68,5 +68,3 @@ assembly_convertor.set_current_format(details_top)
 assembly_convertor.set_secondary_format(details_recap)
 
 assembly_convertor.print(file_type, excel_file, product)
-
-
