9314bf615b4f175a162c6e7b7e754ca6bd705330
evereux <evereux@gmail.com>
2023-08-09 17:14:46 +0100

    tidied up script to make easier to understand and follow python conventions.

--- examples/example_selection_select_element2_002.py
+++ examples/example_selection_select_element2_002.py
@@ -1,10 +1,16 @@
-#! /usr/bin/python3.6
+#! /usr/bin/python3.9
+
 """
-    Example - Selection - 002:
-    Prompt the user to select 2 vertexes and create line in selected geometrical set
-    .. warning:
-        Catia must be run and document open and active.
-        Document must be content 2 vertex(points) and geometrical set!
+
+    Example - Selection - 002
+
+    Description:
+        Prompt the user to select 2 vertexes and create line in selected geometrical set
+
+    Requirements:
+        - CATIA must be running with part document open and active.
+        - Part document must contain 2 vertex (points) and a geometrical set.
+
 """
 
 ##########################################################
@@ -19,48 +25,47 @@ sys.path.insert(0, os.path.abspath('..\\pycatia'))
 
 from pycatia.mec_mod_interfaces.part import Part
 from pycatia import catia
-# import headers
+
 caa = catia()
 documents = caa.documents
 document = caa.active_document
 part = Part(document.part.com_object)
-HybridBodies = part.hybrid_bodies
-
-# create selection
-sel = document.selection
-# Tuple selection 1-D Vertex
-iot = ("Vertex",)
-# Status=Normal is vertex selected
-# True= pre-select is allow
-status = sel.select_element2(iot, "Select the first vertex", True)
-if status=="Cancel":
-    caa.message_box("Canceled",16)
-    exit()
-FirstVertex = sel.item(1).value
-sel.clear()
-
-status = sel.select_element2(iot, "Select the second vertex", True)
-if status=="Cancel":
-    caa.message_box("Canceled",16)
-    exit()
-SecondVertex = sel.item(1).value
-sel.clear()
-
-
+hybrid_bodies = part.hybrid_bodies
 hsf = part.hybrid_shape_factory
 
-# select geometrical set. Preselect is allow
-iot = ("HybridBody",)
-status = sel.select_element2(iot, "Select Geometrical set", True)
-if status=="Cancel":
-    caa.message_box("Canceled",16)
+# create selection object
+selection = document.selection
+
+filter_type = ("Vertex",)
+
+# i_object_selection_before_command_use_possibility - True = pre-selection is allowed.
+status = selection.select_element2(filter_type, "Select the first vertex", True)
+
+# status==Normal is vertex selected
+if status == "Cancel":
+    caa.message_box("Canceled", 16)
+    exit()
+first_vertex = selection.item(1).reference
+selection.clear()
+
+status = selection.select_element2(filter_type, "Select the second vertex", True)
+if status == "Cancel":
+    caa.message_box("Cancelled", 16)
+    exit()
+second_vertex = selection.item(1).reference
+selection.clear()
+
+filter_type = ("HybridBody",)
+status = selection.select_element2(filter_type, "Select Geometrical set", True)
+if status == "Cancel":
+    caa.message_box("Cancelled", 16)
     exit()
 
 # set pointer to selected geometrical set
-hb_construction_geometry = HybridBodies.item(sel.item(1).value.name)
+hb_construction_geometry = hybrid_bodies.get_item_by_name(selection.item(1).value.name)
 
 # create line
-line = hsf.add_new_line_pt_pt(FirstVertex, SecondVertex)
+line = hsf.add_new_line_pt_pt(first_vertex, second_vertex)
 hb_construction_geometry.append_hybrid_shape(line)
 part.update()
-caa.message_box("Line is created!", 0, "Congratulation")
+caa.message_box("Line is created!", 0, "Congratulations!")
