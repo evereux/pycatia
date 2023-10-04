076af803aa7f7c711ad56f1dcacdd53e25803291
evereux <evereux@gmail.com>
2023-10-04 12:33:02 +0100

    update get_source_material for cases where there are multiple paths
    see #139

--- tests/create_source_material.py
+++ tests/create_source_material.py
@@ -4,9 +4,25 @@ from tests.common_vars import caa
 
 
 def get_cat_material():
-    cat_startup_path = caa.application.system_service.environ("CATStartupPath")
+    env_startup_path = caa.application.system_service.environ("CATStartupPath")
     cat_material_filename = "Catalog.CATMaterial"
-    catpart_materials = Path(cat_startup_path, "materials", cat_material_filename)
+
+    cat_startup_paths = []
+    if ";" in env_startup_path:
+        _paths = env_startup_path.split(";")
+        cat_startup_paths = [p.strip() for p in _paths]
+    else:
+        cat_startup_paths.append(env_startup_path.strip())
+
+    catpart_materials = ""
+
+    for cat_startup_path in cat_startup_paths:
+        catpart_materials = Path(cat_startup_path, "materials", cat_material_filename)
+
+        if not catpart_materials.is_file():
+            continue
+        else:
+            break
 
     if not catpart_materials.is_file():
         raise FileNotFoundError(f"Could not find {catpart_materials}.")
