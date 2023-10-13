b7e822836e480e82715a5d102975beb72e1e6064
evereux <evereux@gmail.com>
2023-10-13 09:44:18 +0100

    code clean up and use of enum objects instead of manually doing the work

--- examples/example__selection__003.py
+++ examples/example__selection__003.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*-
 
 """
-Example - example__selection__003
-    Prompt to select geometry
-    returns type of geometry using SPAworkbench get_measurable
-    and hybrid shape factory get_geometrical_feature_type
+    Example - Selection - 003
+
+    Description:
+        Prompts user to select geometry.
+
+        Returns type of geometry using SPAworkbench get_measurable
+        and hybrid shape factory get_geometrical_feature_type
+
+    Requirements:
+        - CATPart open with geometry available to select.
 """
 ##########################################################
 # insert syspath to project folder so examples can be run.
@@ -19,128 +25,66 @@ sys.path.insert(0, os.path.abspath('..\\pycatia'))
 from pycatia.mec_mod_interfaces.part import Part
 from pycatia import catia
 from pycatia.exception_handling.exceptions import CATIAApplicationException
+from pycatia.enumeration.enumeration_types import cat_measurable_name
+from pycatia.enumeration.enumeration_types import cat_selection_filter
+from pycatia.enumeration.enumeration_types import geometrical_feature_type
 
 __author__ = '[ptm] by plm-forum.ru'
 __status__ = 'alpha'
 
-# import headers
-try:
-    caa = catia()
-    documents = caa.documents
-    document = caa.active_document
-except CATIAApplicationException as e:
-    print(e.message)
-    print('CATIA not started or document not ' +
-          'opened or started several CATIA sessions')
-    sys.exit(e.message)
-if document.is_part:
-    # need to autocomplete
-    part_document = Part(document.part.com_object)
-    selection = document.selection
+caa = catia()
+documents = caa.documents
+document = caa.active_document
+part_document = Part(document.part.com_object)
+# Note: It's not necessary to explicitly use the ProductDocument or the Product class
+# with the com_object. It's perfectly fine to write it like this:
+#   document = caa.active_document
+#   product = document.product
+# But declaring 'document' and 'product' this way, your linter can't resolve the
+# product reference, see https://github.com/evereux/pycatia/issues/107#issuecomment-1336195688
 
-    try:
-        part_document.update()
-    except CATIAApplicationException as e:
-        print(e.message)
-        print('Part document must be without errors!')
-        sys.exit('Part document must be without errors!')
-else:
-    print('This script worked in Part only!')
-    sys.exit('Not a part!')
+selection = document.selection
+selection.clear()
 
 hsf = part_document.hybrid_shape_factory
 
 spa = document.spa_workbench()
 
 # promt user select any object
-caa.message_box('Select a anyobjects.\n'
-                'You can select in tree or graphical area\n'
+caa.message_box('Select any HybridShape, or PartBody.\n'
+                'You can select in tree or graphical area.\n'
                 'If you select in graphical area script has some error',
-                0, title='Selection promt')
+                0, title='Selection prompt')
 
-# type geom filter
-sFilter = ('ZeroDim',
-           'MonoDim',
-           'MonoDimInfinite',
-           'RectilinearMonoDim',
-           'RectilinearMonoDimInfinite',
-           'BiDim',
-           'BiDimInfinite',
-           'PlanarBiDim',
-           'PlanarBiDimInfinite',
-           'CylindricalBiDim',
-           'TriDim'
-           )
-
-sStatus = selection.select_element2(sFilter, 'select a HybridBody', True)
-if sStatus == 'Cancel':
-    sys.exit('HybridBodies not select')
+selected = selection.select_element2(cat_selection_filter, 'Select a HybridShape or PartBody.', True)
+if selected == 'Cancel':
+    sys.exit('HybridShape or PartBody not selected.')
 
 sel_name = selection.item2(1).value.name
 parent_name = selection.item2(1).value.parent.name
 type_sel = selection.item2(1).type
 
-
 ref_hb = selection.item2(1).reference
 
+# I'm not sure this try / except clause is neccessary? Are there cases where
+# CATIA will not report an int? :evereux
 try:
-    int_geom_add_type = spa.get_measurable(ref_hb).geometry_name
-    print(int_geom_add_type)
-    match int_geom_add_type:
-        case 0:
-            ADD_GEOM_TYPE = 'Unknown'
-        case 1:
-            ADD_GEOM_TYPE = 'Other'
-        case 2:
-            ADD_GEOM_TYPE = 'Volume'
-        case 3:
-            ADD_GEOM_TYPE = 'Surface'
-        case 4:
-            ADD_GEOM_TYPE = 'Cylinder'
-        case 5:
-            ADD_GEOM_TYPE = 'Sphere'
-        case 6:
-            ADD_GEOM_TYPE = 'Cone'
-        case 7:
-            ADD_GEOM_TYPE = 'Plane'
-        case 8:
-            ADD_GEOM_TYPE = 'Curve'
-        case 9:
-            ADD_GEOM_TYPE = 'Circle'
-        case 10:
-            ADD_GEOM_TYPE = 'Line'
-        case 11:
-            ADD_GEOM_TYPE = 'Point'
-        case 12:
-            ADD_GEOM_TYPE = 'Axis System'
+    geom_add_type: int = spa.get_measurable(ref_hb).geometry_name
+    add_geom_type: str = cat_measurable_name[geom_add_type]
 except:
-    ADD_GEOM_TYPE = None
+    add_geom_type = cat_measurable_name[0]
+
+# I'm not sure this try / except clause is neccessary? Are there cases where
+# CATIA will not report an int? :evereux
 try:
-    int_geom_base_type = hsf.get_geometrical_feature_type(ref_hb)
-
-    match int_geom_base_type:
-        case 0:
-            BASE_GEOM_TYPE = 'Unknown'
-        case 1:
-            BASE_GEOM_TYPE = 'Point'
-        case 2:
-            BASE_GEOM_TYPE = 'Curve'
-        case 3:
-            BASE_GEOM_TYPE = 'Line'
-        case 4:
-            BASE_GEOM_TYPE = 'Circle'
-        case 5:
-            BASE_GEOM_TYPE = 'Surface'
-        case 6:
-            BASE_GEOM_TYPE = 'Plane'
-        case 7:
-            BASE_GEOM_TYPE = 'Solid or Volume'
+    geom_base_type: int = hsf.get_geometrical_feature_type(ref_hb)
+    base_geom_type: str = geometrical_feature_type(geom_base_type)
 except:
-    BASE_GEOM_TYPE = None
+    base_geom_type = geometrical_feature_type[0]
 
-text = f'selection_name={sel_name}\n'\
-    f'parent={parent_name}\n'\
-    f'base_geom_type={BASE_GEOM_TYPE}\n'\
-    f'addition_geom_type={ADD_GEOM_TYPE}'
+text = f'selection_name={sel_name}\n' \
+       f'parent={parent_name}\n' \
+       f'base_geom_type={base_geom_type}\n' \
+       f'addition_geom_type={add_geom_type}'
 caa.message_box(text)
-sys.exit(text)
+print(text)
