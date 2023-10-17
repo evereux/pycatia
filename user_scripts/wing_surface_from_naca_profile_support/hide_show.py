88fc4ac4e08435aeff019fe8ba4bb60d8d931233
evereux <evereux@gmail.com>
2023-10-17 12:41:10 +0100

    code improvements for wing_surface_from_naca_profile

--- user_scripts/wing_surface_from_naca_profile_support/hide_show.py
+++ user_scripts/wing_surface_from_naca_profile_support/hide_show.py
@@ -1,17 +1,16 @@
-from pycatia.in_interfaces.document import Document
+from pycatia.in_interfaces.selection import Selection
 from pycatia.mec_mod_interfaces.hybrid_body import HybridBody
 from pycatia.mec_mod_interfaces.hybrid_shape import HybridShape
 
 
-def hide_the_shape(document: Document, hide_elements: list[HybridShape]) -> None:
+def hide_the_shape(selection: Selection, hide_elements: list[HybridShape]) -> None:
     """
 
-    :param Document document:
+    :param Selection document:
     :param HybridShape hybrid_shape:
     :return:
     """
 
-    selection = document.selection
     visual_properties = selection.vis_properties
     for hs in hide_elements:
         selection.add(hs)
@@ -19,12 +18,12 @@ def hide_the_shape(document: Document, hide_elements: list[HybridShape]) -> None
     selection.clear()
 
 
-def hide_the_shapes(document: Document, hybrid_body: HybridBody) -> None:
+def hide_the_shapes(selection: Selection, hybrid_body: HybridBody) -> None:
     """
     Hides all the HybridShapes under HybridBody. Recursively gets all
     HybridShapes to hide.
-    :param document:
-    :param hybrid_body:
+    :param Selection selection:
+    :param HybridBody hybrid_body:
     :return:
     """
 
@@ -34,9 +33,9 @@ def hide_the_shapes(document: Document, hybrid_body: HybridBody) -> None:
     hide_collection = []
 
     for hybrid_body in hybrid_bodies:
-        hide_the_shapes(document, hybrid_body)
+        hide_the_shapes(selection, hybrid_body)
 
     for hybrid_shape in hybrid_shapes:
         hide_collection.append(hybrid_shape)
 
-    hide_the_shape(document, hide_collection)
+    hide_the_shape(selection, hide_collection)
