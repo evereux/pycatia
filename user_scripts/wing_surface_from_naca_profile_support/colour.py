ebf913285b2f32b5b2e23290ee15943aa6cacece
Philip Delorenzo <git@deloarts.com>
2023-10-17 14:27:59 +0200

    Simplify params

--- user_scripts/wing_surface_from_naca_profile_support/colour.py
+++ user_scripts/wing_surface_from_naca_profile_support/colour.py
@@ -2,11 +2,7 @@ from pycatia.in_interfaces.selection import Selection
 from pycatia.system_interfaces.any_object import AnyObject
 
 
-def set_colour(
-        selection: Selection,
-        element: AnyObject,
-        colour: tuple[int, int, int, int]
-) -> None:
+def set_colour(selection: Selection, element: AnyObject, colour: tuple[int, int, int, int]) -> None:
     """
 
     :param Selection selection:
@@ -14,13 +10,8 @@ def set_colour(
     :param tuple[int, int, int, int] colour:
     :return:
     """
-    red = colour[0]
-    green = colour[1]
-    blue = colour[2]
-    inheritance = colour[3]
-
     selection.clear()
     selection.add(element)
     visual_properties = selection.vis_properties
-    visual_properties.set_real_color(0, 255, 0, 0)
+    visual_properties.set_real_color(*colour)
     selection.clear()
