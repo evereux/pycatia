b916e2d1aea6a9c07939c0c976c514b9fd6024e2
Mithro86 <84014236+Mithro86@users.noreply.github.com>
2024-01-05 12:40:09 +0100

    Update coords_relative_to_axis_system.py

--- user_scripts/coords_relative_to_axis_system.py
+++ user_scripts/coords_relative_to_axis_system.py
@@ -30,41 +30,41 @@ hybrid_bodies = part.hybrid_bodies
 spa_workbench = document.spa_workbench()
 
 def coords_relative_to_axis(axis_system, point):
-	a_origin = axis_system.get_origin()
-	a_xaxis = axis_system.get_x_axis()
-	a_yaxis = axis_system.get_y_axis()
-	a_zaxis = axis_system.get_z_axis()
+    a_origin = axis_system.get_origin()
+    a_xaxis = axis_system.get_x_axis()
+    a_yaxis = axis_system.get_y_axis()
+    a_zaxis = axis_system.get_z_axis()
 
-	n_x = normalize_vector(a_xaxis)
-	n_y = normalize_vector(a_yaxis)
-	n_z = normalize_vector(a_zaxis)
+    n_x = normalize_vector(a_xaxis)
+    n_y = normalize_vector(a_yaxis)
+    n_z = normalize_vector(a_zaxis)
 
-  reference = part.create_reference_from_object(point)
-	measurable = spa_workbench.get_measurable(reference)
-	coordinates = measurable.get_point()
+    reference = part.create_reference_from_object(point)
+    measurable = spa_workbench.get_measurable(reference)
+    coordinates = measurable.get_point()
 
-	diff = [0] * 3
-	diff[0] = coordinates[0] - a_origin[0]
-	diff[1] = coordinates[1] - a_origin[1]
-	diff[2] = coordinates[2] - a_origin[2]
+    diff = [0] * 3
+    diff[0] = coordinates[0] - a_origin[0]
+    diff[1] = coordinates[1] - a_origin[1]
+    diff[2] = coordinates[2] - a_origin[2]
 
-	x = dot_product(diff, a_xaxis) 
-	y = dot_product(diff, a_yaxis)
-	z = dot_product(diff, a_zaxis)
+    x = dot_product(diff, a_xaxis) 
+    y = dot_product(diff, a_yaxis)
+    z = dot_product(diff, a_zaxis)
 
-	return (x, y, z)
+    return (x, y, z)
 
 def normalize_vector(vec):
     magnitude = (vec[0]**2 + vec[1]**2 + vec[2]**2)**0.5
     if magnitude != 0:
-    	x = vec[0] / magnitude
+        x = vec[0] / magnitude
     	y = vec[1] / magnitude
     	z = vec[2] / magnitude
 
     	return (x, y, z)
 
 def dot_product(vec1, vec2):
-	return vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2]
+    return vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2]
 
 # Get first axis system in collection
 axis_system = part.axis_systems.item(1)
