14ddbf122b5022824e5d030fd5407204b3a38882
evereux <evereux@gmail.com>
2024-04-18 11:06:51 +0100

    added ability to Dispatch CATIA.Application with pythoncon.CoInitialize() this was required for an app I'm developing and will probably help others having the same threading issues

--- pycatia/base_interfaces/base_application.py
+++ pycatia/base_interfaces/base_application.py
@@ -1,9 +1,12 @@
 #! /usr/bin/python3.9
 
-from win32com.client import Dispatch
+from win32com.client import Dispatch, gencache
+import pythoncom
 
 from pycatia.in_interfaces.application import Application
 
 
-def catia_application() -> Application:
+def catia_application(co_initialise=False) -> Application:
+    if co_initialise:
+        return Application(Dispatch('CATIA.Application', pythoncom.CoInitialize()))
     return Application(Dispatch('CATIA.Application'))
