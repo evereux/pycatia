3310d4face37915f187bd4e2af2979bb4340b29c
evereux <evereux@gmail.com>
2025-05-18 17:46:33 +0100

    added tests for Plane() class
    not all methods are covered yet

--- tests/in/hsf/test_hsf_plane.py
+++ tests/in/hsf/test_hsf_plane.py
@@ -6,50 +6,113 @@ from tests.source_files import cat_part_measurable
 
 
 def test_plane_get_first_axis():
-    # todo: write test
-    pass
+    with CATIADocHandler(cat_part_measurable) as caa:
+        part_document: PartDocument = caa.document
+        part = part_document.part
+        hybrid_bodies = part.hybrid_bodies
+        hb_construction_planes = hybrid_bodies.item('construction_planes')
+        hybrid_shapes = hb_construction_planes.hybrid_shapes
+        plane_1 = Plane(hybrid_shapes.item('Plane.Offset').com_object)
+        first_axis = plane_1.get_first_axis()
+
+        assert (first_axis == (1.0, 0.0, 0.0))
 
 
 def test_plane_get_origin():
-    # todo: write test
-    pass
+    with CATIADocHandler(cat_part_measurable) as caa:
+        part_document: PartDocument = caa.document
+        part = part_document.part
+        hybrid_bodies = part.hybrid_bodies
+        hb_construction_planes = hybrid_bodies.item('construction_planes')
+        hybrid_shapes = hb_construction_planes.hybrid_shapes
+        plane_1 = Plane(hybrid_shapes.item('Plane.TwoLines').com_object)
+        origin = plane_1.get_origin()
+
+        assert (origin == (50.000000000000014, 50.000000000000014, 0.0))
 
 
 def test_plane_get_position():
-    # todo: write test
-    pass
+    with CATIADocHandler(cat_part_measurable) as caa:
+        part_document: PartDocument = caa.document
+        part = part_document.part
+        hybrid_bodies = part.hybrid_bodies
+        hb_construction_planes = hybrid_bodies.item('construction_planes')
+        hybrid_shapes = hb_construction_planes.hybrid_shapes
+        plane_1 = Plane(hybrid_shapes.item('Plane.Offset').com_object)
+        position = plane_1.get_position()
+
+        assert (position == (0.0, 0.0, 0.0))
 
 
 def test_plane_get_second_axis():
-    # todo: write test
-    pass
+    with CATIADocHandler(cat_part_measurable) as caa:
+        part_document: PartDocument = caa.document
+        part = part_document.part
+        hybrid_bodies = part.hybrid_bodies
+        hb_construction_planes = hybrid_bodies.item('construction_planes')
+        hybrid_shapes = hb_construction_planes.hybrid_shapes
+        plane_1 = Plane(hybrid_shapes.item('Plane.Offset').com_object)
+        position = plane_1.get_position()
+
+        assert (position == (0.0, 0.0, 0.0))
 
 
 def test_plane_is_a_ref_plane():
-    # todo: write test
-    pass
+    with CATIADocHandler(cat_part_measurable) as caa:
+        part_document: PartDocument = caa.document
+        part = part_document.part
+        hybrid_bodies = part.hybrid_bodies
+        hb_construction_planes = hybrid_bodies.item('construction_planes')
+        hybrid_shapes = hb_construction_planes.hybrid_shapes
+        plane_1 = Plane(hybrid_shapes.item('Plane.Offset').com_object)
+        is_ref_plane = plane_1.is_a_ref_plane()
+
+        assert (is_ref_plane == 1)
 
 
-def test_plane_put_first_axis():
-    # todo: write test
-    pass
-
-
-def test_plane_put_origin():
-    # todo: write test
-    pass
-
-
-def test_plane_put_second_axis():
-    # todo: write test
-    pass
+# def test_plane_put_first_axis():
+#     # todo: write test
+#     pass
+#
+#
+# def test_plane_put_origin():
+#     # todo: write test
+#     pass
+#
+#
+# def test_plane_put_second_axis():
+#     # todo: write test
+#     pass
 
 
 def test_plane_remove_position():
-    # todo: write test
-    pass
+    with CATIADocHandler(cat_part_measurable) as caa:
+        part_document: PartDocument = caa.document
+        part = part_document.part
+        hybrid_bodies = part.hybrid_bodies
+        hb_construction_planes = hybrid_bodies.item('construction_planes')
+        hybrid_shapes = hb_construction_planes.hybrid_shapes
+
+        plane_1 = Plane(hybrid_shapes.item('Plane.TwoLines').com_object)
+
+        position = (1.0, 1.0, 0.0)
+        plane_1.set_position(*position)
+        part.update()
+        plane_1.remove_position()
+
+        assert (plane_1.get_position() == (0.0, 0.0, 0.0))
 
 
 def test_plane_set_position():
-    # todo: write test
-    pass
+    with CATIADocHandler(cat_part_measurable) as caa:
+        part_document: PartDocument = caa.document
+        part = part_document.part
+        hybrid_bodies = part.hybrid_bodies
+        hb_construction_planes = hybrid_bodies.item('construction_planes')
+        hybrid_shapes = hb_construction_planes.hybrid_shapes
+        plane_1 = Plane(hybrid_shapes.item('Plane.TwoLines').com_object)
+        position = (1.0, 1.0, 0.0)
+        plane_1.set_position(*position)
+        location = plane_1.get_position()
+
+        assert (location == (position))
