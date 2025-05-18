abd7f10cc8cffcc8b92a85a4be8b491899a34e0f
evereux <evereux@gmail.com>
2025-05-18 08:58:21 +0100

    create skeleton for plane tests

---
+++ tests/in/hsf/test_hsf_plane.py
@@ -0,0 +1,55 @@
+from pycatia import CATIADocHandler
+from pycatia.mec_mod_interfaces.part_document import PartDocument
+from pycatia.hybrid_shape_interfaces.plane import Plane
+
+from tests.source_files import cat_part_measurable
+
+
+def test_plane_get_first_axis():
+    # todo: write test
+    pass
+
+
+def test_plane_get_origin():
+    # todo: write test
+    pass
+
+
+def test_plane_get_position():
+    # todo: write test
+    pass
+
+
+def test_plane_get_second_axis():
+    # todo: write test
+    pass
+
+
+def test_plane_is_a_ref_plane():
+    # todo: write test
+    pass
+
+
+def test_plane_put_first_axis():
+    # todo: write test
+    pass
+
+
+def test_plane_put_origin():
+    # todo: write test
+    pass
+
+
+def test_plane_put_second_axis():
+    # todo: write test
+    pass
+
+
+def test_plane_remove_position():
+    # todo: write test
+    pass
+
+
+def test_plane_set_position():
+    # todo: write test
+    pass
