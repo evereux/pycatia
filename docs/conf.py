749a8bc32af11a95ac5f8901aa44bfa13d026f0b
evereux <evereux@gmail.com>
2025-08-15 09:56:58 +0100

    version bump

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ copyright = '2025, Paul Bourne'
 author = 'Paul Bourne'
 
 # The short X.Y version
-version = '0.8.3'
+version = '0.9.1'
 # The full version, including alpha/beta/rc tags
 release = version
 
